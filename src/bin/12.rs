ecff10e7e6cc75967d0102b927e3a5e6b9a38db6
uflowie <bananasawsm@gmail.com>
2024-12-13 17:14:54 +0100

    day 12 second draft

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -5,26 +5,17 @@ use advent_of_code::{add, get_index_neighbors, indexed_chars};
 advent_of_code::solution!(12);
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let plants = indexed_chars(input);
-    let mut visited_indices: HashSet<(i32, i32)> = HashSet::new();
-    let mut regions = vec![];
-
-    for &idx in plants.keys() {
-        if visited_indices.contains(&idx) {
-            continue;
-        }
-        let region = Region::new(idx, &plants);
-        visited_indices.extend(&region.occupied_indices);
-        regions.push(region);
-    }
-
-    Some(regions.iter().map(Region::price).sum())
+    Some(solve(input, Region::price))
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
+    Some(solve(input, Region::discounted_price))
+}
+
+fn solve(input: &str, price_f: fn(&Region) -> usize) -> usize {
     let plants = indexed_chars(input);
-    let mut visited_indices: HashSet<(i32, i32)> = HashSet::new();
-    let mut regions = vec![];
+    let mut visited_indices = HashSet::<(i32, i32)>::new();
+    let mut regions = Vec::new();
 
     for &idx in plants.keys() {
         if visited_indices.contains(&idx) {
@@ -35,45 +26,36 @@ pub fn part_two(input: &str) -> Option<usize> {
         regions.push(region);
     }
 
-    Some(regions.iter().map(Region::discounted_price).sum())
+    regions.iter().map(price_f).sum()
 }
 
 struct Region {
-    plant: char,
     occupied_indices: HashSet<(i32, i32)>,
 }
 
 impl Region {
-    fn new(start: (i32, i32), all_plants: &HashMap<(i32, i32), char>) -> Region {
+    fn new(start: (i32, i32), all_plants: &HashMap<(i32, i32), char>) -> Self {
         let mut occupied_indices = HashSet::from([start]);
         let mut remaining = VecDeque::from([start]);
-        let plant = *all_plants.get(&start).unwrap();
+        let plant = all_plants[&start];
 
         while let Some(next) = remaining.pop_back() {
             for neighbor in get_index_neighbors(next) {
-                if let Some(_) = occupied_indices.get(&neighbor) {
+                if occupied_indices.contains(&neighbor) {
                     continue;
                 }
-
-                match all_plants.get(&neighbor) {
-                    Some(&ch) if ch == plant => {
-                        occupied_indices.insert(neighbor);
-                        remaining.push_back(neighbor);
-                    }
-                    _ => continue,
+                if all_plants.get(&neighbor) == Some(&plant) {
+                    occupied_indices.insert(neighbor);
+                    remaining.push_back(neighbor);
                 }
             }
         }
 
-        Self {
-            plant,
-            occupied_indices,
-        }
+        Self { occupied_indices }
     }
 
     fn price(&self) -> usize {
         let area = self.occupied_indices.len();
-
         let circumference: usize = self
             .occupied_indices
             .iter()
@@ -84,119 +66,72 @@ impl Region {
                     .count()
             })
             .sum();
-
         area * circumference
     }
 
     fn discounted_price(&self) -> usize {
         let area = self.occupied_indices.len();
 
-        let adjacent_indices: HashSet<(i32, i32)> = self
+        let adjacent_indices: HashSet<_> = self
             .occupied_indices
             .iter()
             .flat_map(|&idx| get_index_neighbors(idx))
             .filter(|idx| !self.occupied_indices.contains(idx))
             .collect();
 
-        let convex_turns = self
-            .occupied_indices
-            .iter()
-            .map(|&idx| {
-                let adjacent_neighbors: Vec<_> = get_index_neighbors(idx)
-                    .into_iter()
-                    .filter(|&idx| adjacent_indices.contains(&idx))
-                    .collect();
-                if adjacent_neighbors.len() == 4 {
-                    4
-                } else if adjacent_neighbors.len() == 3 {
-                    2
-                } else if adjacent_neighbors.len() == 2 {
-                    let ((a, b), (c, d)) = (adjacent_neighbors[0], adjacent_neighbors[1]);
-                    if a == c || b == d {
-                        0
-                    } else {
-                        1
-                    }
-                } else {
-                    0
-                }
-            })
-            .sum::<usize>();
-
-        let concave_turns = adjacent_indices
-            .iter()
-            .map(|&idx| {
-                let adjacent_neighbors: Vec<_> = get_index_neighbors(idx)
-                    .into_iter()
-                    .filter(|&idx| self.occupied_indices.contains(&idx))
-                    .collect();
-                if adjacent_neighbors.len() == 4 {
-                    4
-                } else if adjacent_neighbors.len() == 3 {
-                    2
-                } else if adjacent_neighbors.len() == 2 {
-                    let ((a, b), (c, d)) = (adjacent_neighbors[0], adjacent_neighbors[1]);
-                    if a == c || b == d {
-                        0
-                    } else {
-                        1
-                    }
-                } else {
-                    0
-                }
-            })
-            .sum::<usize>();
-
-        // get double counts:
+        let convex_turns = calc_turns(&self.occupied_indices, &adjacent_indices);
+        let concave_turns = calc_turns(&adjacent_indices, &self.occupied_indices);
 
         let double_counts = self
             .occupied_indices
             .iter()
-            .map(|idx| {
-                let mut counts = 0;
-
-                let bottom_right_neighbors = [
-                    *idx,
-                    add(*idx, (1, 0)),
-                    add(*idx, (1, -1)),
-                    add(*idx, (0, -1)),
-                ]
-                .map(|idx| self.occupied_indices.contains(&idx));
-
-                if bottom_right_neighbors == [true, false, true, false] {
-                    counts += 1;
-                }
-
-                let bottom_right_neighbors = [
-                    *idx,
-                    add(*idx, (-1, 0)),
-                    add(*idx, (-1, -1)),
-                    add(*idx, (0, -1)),
-                ]
-                .map(|idx| self.occupied_indices.contains(&idx));
-
-                if bottom_right_neighbors == [true, false, true, false] {
-                    counts += 1;
-                }
-
-                counts
+            .map(|&idx| {
+                let count = |offsets: [(i32, i32); 4]| {
+                    let pattern: Vec<_> = offsets
+                        .iter()
+                        .map(|&off| self.occupied_indices.contains(&add(idx, off)))
+                        .collect();
+                    if pattern == [true, false, true, false] {
+                        1
+                    } else {
+                        0
+                    }
+                };
+                (count([(0, 0), (1, 0), (1, -1), (0, -1)])
+                    + count([(0, 0), (-1, 0), (-1, -1), (0, -1)]))
+                    * 2
             })
-            .sum::<usize>()
-            * 2;
+            .sum::<usize>();
 
         let sides = convex_turns + concave_turns - double_counts;
 
-        println!(
-            "plant: {}, convex turns: {}, concave turns: {}, price: {}, double counts: {}",
-            self.plant,
-            convex_turns,
-            concave_turns,
-            area * (sides),
-            double_counts
-        );
-
-        area * (sides)
+        area * sides
     }
+}
+
+fn calc_turns(base_set: &HashSet<(i32, i32)>, check_set: &HashSet<(i32, i32)>) -> usize {
+    base_set
+        .iter()
+        .map(|&idx| {
+            let neighbors: Vec<_> = get_index_neighbors(idx)
+                .into_iter()
+                .filter(|pos| check_set.contains(pos))
+                .collect();
+            match neighbors.len() {
+                4 => 4,
+                3 => 2,
+                2 => {
+                    let ((a, b), (c, d)) = (neighbors[0], neighbors[1]);
+                    if a == c || b == d {
+                        0
+                    } else {
+                        1
+                    }
+                }
+                _ => 0,
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
